64002d97d8382940999ae8d793b7b4adacc0119a
Matt Wrench <matt@mattwrench.com>
2020-11-21 12:55:31 -0600

    Draw to RenderTarget2D then to window

--- Views/Renderer.cs
+++ Views/Renderer.cs
@@ -15,12 +15,14 @@ namespace Dodgeball.Views
         private SpriteBatch spriteBatch;
         private GraphicsDevice graphicsDevice;
         private World world;
+        private RenderTarget2D renderTarget;
 
         public Renderer(GraphicsDeviceManager graphics, World world)
         {
             graphicsDevice = graphics.GraphicsDevice;
             spriteBatch = new SpriteBatch(graphicsDevice);
             this.world = world;
+            renderTarget = new RenderTarget2D(graphicsDevice, world.Width, world.Height);
 
             // Set window size & title
             graphics.PreferredBackBufferWidth = WindowWidth;
@@ -32,8 +34,16 @@ namespace Dodgeball.Views
         {
             graphicsDevice.Clear(Color.Black);
 
+            // Draw to render target
+            graphicsDevice.SetRenderTarget(renderTarget);
             spriteBatch.Begin();
 
+            spriteBatch.End();
+
+            // Draw render target to window
+            graphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin();
+            spriteBatch.Draw(renderTarget, new Rectangle(0, 0, WindowWidth, WindowHeight), Color.White);
             spriteBatch.End();
         }
     }
