ef484468fc4d72ea607f99d06821c1d69ee552a9
Matt Wrench <matt@mattwrench.com>
2020-11-22 15:55:34 -0600

    Add GameCharController class

---
+++ Controllers/GameCharController.cs
@@ -0,0 +1,19 @@
+﻿using Dodgeball.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodgeball.Controllers
+{
+    abstract class GameCharController : EntityController
+    {
+        protected GameCharController(World world) : base(world)
+        {
+        }
+
+        protected override void boundsCheck(Entity entity)
+        {
+            // TODO
+        }
+    }
+}
