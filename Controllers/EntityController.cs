ce03f05128df65ddcb75b5ea6a52541b6a79df87
Matt Wrench <matt@mattwrench.com>
2020-11-22 15:54:04 -0600

    Add EntityController class

---
+++ Controllers/EntityController.cs
@@ -0,0 +1,28 @@
+﻿using Dodgeball.Models;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodgeball.Controllers
+{
+    abstract class EntityController
+    {
+        protected World world;
+
+        public EntityController(World world)
+        {
+            this.world = world;
+        }
+
+        public abstract void Update(float dt);
+        protected abstract void setVelocity(Entity entity, float dt);
+        protected abstract void boundsCheck(Entity entity);
+
+        // Add velocity * dt to position
+        protected void setPosition(Entity entity, float dt)
+        {
+            entity.Position = Vector2.Add(entity.Position, Vector2.Multiply(entity.Velocity, dt));
+        }
+    }
+}
