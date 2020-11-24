5bd0a3faf0b58e909a3e7da2aa08c1506ac6e2f4
Matt Wrench <matt@mattwrench.com>
2020-11-23 19:30:40 -0600

    Add Ball class

---
+++ Models/Ball.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodgeball.Models
+{
+    class Ball : Entity
+    {
+        public bool IsAlive;
+
+        public Ball(Vector2 position, Vector2 direction, bool alive)
+        {
+            // Class attributes
+            Bounds.Width = 32;
+            Bounds.Height = 32;
+            TopSpeed = 400;
+
+            IsAlive = alive;
+
+            Position.X = position.X;
+            Position.Y = position.Y;
+
+            Velocity = Vector2.Multiply(direction, TopSpeed);
+
+            SetBounds();
+        }
+    }
+}
