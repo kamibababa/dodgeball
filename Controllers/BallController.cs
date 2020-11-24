1911565ccdb03304acd6ba8624472eceb37a5514
Matt Wrench <matt@mattwrench.com>
2020-11-23 19:43:34 -0600

    Add BallController class

---
+++ Controllers/BallController.cs
@@ -0,0 +1,41 @@
+﻿using Dodgeball.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodgeball.Controllers
+{
+    class BallController : EntityController
+    {
+        public BallController(World world) : base(world)
+        {
+        }
+
+        public override void Update(float dt)
+        {
+            foreach (Ball ball in world.Balls)
+            {
+                setVelocity(ball, dt);
+                setPosition(ball, dt);
+                boundsCheck(ball);
+                collisionDetect(ball);
+            }
+        }
+
+        // Handle collisions between ball and GameChars
+        private void collisionDetect(Ball ball)
+        {
+            // TODO
+        }
+
+        protected override void boundsCheck(Entity entity)
+        {
+            // TODO
+        }
+
+        protected override void setVelocity(Entity entity, float dt)
+        {
+            // TODO
+        }
+    }
+}
--- Controllers/ControllerSet.cs
+++ Controllers/ControllerSet.cs
@@ -9,15 +9,18 @@ namespace Dodgeball.Controllers
     class ControllerSet
     {
         private PlayerController playerController;
+        private BallController ballController;
 
         public ControllerSet(World world)
         {
             playerController = new PlayerController(world);
+            ballController = new BallController(world);
         }
 
         public void Update(float dt)
         {
             playerController.Update(dt);
+            ballController.Update(dt);
         }
     }
 }
