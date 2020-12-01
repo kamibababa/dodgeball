647a8349a70e8180f200d079ed6ea2d94823da32
Matt Wrench <matt@mattwrench.com>
2020-11-30 20:17:16 -0600

    Add Screen class

---
+++ Screens/Screen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodgeball
+{
+    abstract class Screen
+    {
+        public abstract void Initialize();
+        public abstract void Update(float dt);
+        public abstract void Render(float dt);
+    }
+}
